a9a39c381f249de9411da86c4f06b21b7456e6b5
PhBouzid <sindbad342@gmail.com>
2018-10-17 17:59:48 +0600

    add fileserver

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 
 func main(){
 	http.HandleFunc("/audio",Audiohandler)
+
+	fs := http.FileServer(http.Dir("./web"))
+	http.Handle("/web/", fs)
+	http.Handle("/", fs)
 	http.ListenAndServe(":7008",nil)
 }
 
