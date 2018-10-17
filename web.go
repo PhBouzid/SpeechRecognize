b3df6ff92cdad314905ec0e3ada36f7c31cb458d
PhBouzid <sindbad342@gmail.com>
2018-10-17 18:21:11 +0600

    add fileserver

--- web.go
+++ web.go
@@ -42,4 +42,37 @@ func Audiohandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func AudiNo(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Connection", "Keep-Alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Transfer-Encoding", "chunked")
+	w.Header().Set("Content-Type", "audio/mpeg")
+	w.Header().Set("ice-audio-info","bitrate=128")
+	w.Header().Set("icy-br","128")
+	w.Header().Set("icy-description","Default description")
+	w.Header().Set("icy-genre","Unspecified")
+	w.Header().Set("icy-name","RFM Demo Stream")
+	w.Header().Set("icy-pub","0")
+	for true {
+		files, err := ioutil.ReadDir("./music")
+		if err != nil {
+			logerror(err)
+		}
+
+		for _, file := range files {
+			fmt.Println(file.Name())
+			b,err:=ioutil.ReadFile("./music/"+file.Name())
+			if err!=nil{
+				logerror(err)
+			}
+			binary.Write(w, binary.BigEndian,b)
+			//flusher.Flush() // Trigger "chunked" encoding
+		}
+		return
+	}
+}
+
+
+
 
