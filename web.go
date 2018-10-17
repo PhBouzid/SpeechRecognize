c7a8088e3dc439b09a4f54e4d6a8be3bf2d37032
PhBouzid <sindbad342@gmail.com>
2018-10-17 20:33:28 +0600

    add fileserver

--- web.go
+++ web.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"encoding/binary"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
+	"encoding/binary"
+	"io/ioutil"
+	"fmt"
+	"bufio"
+	"io"
+	"log"
 	"os"
 )
 
 const (
-	chunksize int = 44100 * 2
+	chunksize int =  44100 * 2
 )
 
 func openFile(name string, buffer []byte) (byteCount int) {
@@ -38,75 +38,70 @@ func openFile(name string, buffer []byte) (byteCount int) {
 }
 
 func Audiohandler(w http.ResponseWriter, r *http.Request) {
-	// flusher, ok := w.(http.Flusher)
-	// if !ok {
-	// 	logwarn("expected http.ResponseWriter to be an http.Flusher")
-	// }
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		logwarn("expected http.ResponseWriter to be an http.Flusher")
+	}
 	w.Header().Set("Connection", "Keep-Alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.Header().Set("Transfer-Encoding", "chunked")
 	w.Header().Set("Content-Type", "audio/mpeg")
-	w.Header().Set("ice-audio-info", "bitrate=128")
-	w.Header().Set("icy-br", "128")
-	w.Header().Set("icy-description", "Default description")
-	w.Header().Set("icy-genre", "Unspecified")
-	w.Header().Set("icy-name", "RFM Demo Stream")
-	w.Header().Set("icy-pub", "0")
-	w.Header().Set("ice-audio-info", "bitrate=128")
-	w.Header().Set("icy-br", "128")
-	w.Header().Set("icy-description", "Default description")
-	w.Header().Set("icy-genre", "Unspecified")
-	w.Header().Set("icy-name", "RFM Demo Stream")
-	w.Header().Set("icy-pub", "0")
-	buffer := make([]byte, 44100*2)
-	go func() {
-		for true {
-			files, err := ioutil.ReadDir("./music")
-			if err != nil {
-				logerror(err)
-			}
-			for _, file := range files {
-				fmt.Println(file.Name())
-				openFile("./music/"+file.Name(), buffer)
-				if err != nil {
-					logerror(err)
-				}
-				//
-			}
-		}
-	}()
+	w.Header().Set("ice-audio-info","bitrate=128")
+	w.Header().Set("icy-br","128")
+	w.Header().Set("icy-description","Default description")
+	w.Header().Set("icy-genre","Unspecified")
+	w.Header().Set("icy-name","RFM Demo Stream")
+	w.Header().Set("icy-pub","0")
+	buffer := make([]byte, 44100 * 2)
 	for true {
 		files, err := ioutil.ReadDir("./music")
 		if err != nil {
 			logerror(err)
 		}
-
 		for _, file := range files {
 			fmt.Println(file.Name())
-			b, err := ioutil.ReadFile("./music/" + file.Name())
+			data, err := os.Open("./music/"+file.Name())
 			if err != nil {
-				logerror(err)
+				log.Fatal(err)
 			}
-			binary.Write(w, binary.BigEndian, b)
-			//flusher.Flush() // Trigger "chunked" encoding
+			defer data.Close()
+
+			reader := bufio.NewReader(data)
+			for {
+				count, err:=reader.Read(buffer)
+				if(err!=nil){
+					logerror(err)
+				}
+				if(count==-1){
+					break
+				}
+				if err != io.EOF {
+					log.Fatal("Error Reading ", file.Name(), ": ", err)
+				} else {
+					err = nil
+				}
+				binary.Write(w, binary.BigEndian, buffer)
+				flusher.Flush() //
+			}
 		}
+
 		return
 	}
 }
 
-func AudiNo(w http.ResponseWriter, r *http.Request) {
+func AudiNo(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Connection", "Keep-Alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.Header().Set("Transfer-Encoding", "chunked")
 	w.Header().Set("Content-Type", "audio/mpeg")
-	w.Header().Set("ice-audio-info", "bitrate=128")
-	w.Header().Set("icy-br", "128")
-	w.Header().Set("icy-description", "Default description")
-	w.Header().Set("icy-genre", "Unspecified")
-	w.Header().Set("icy-name", "RFM Demo Stream")
-	w.Header().Set("icy-pub", "0")
+	w.Header().Set("ice-audio-info","bitrate=128")
+	w.Header().Set("icy-br","128")
+	w.Header().Set("icy-description","Default description")
+	w.Header().Set("icy-genre","Unspecified")
+	w.Header().Set("icy-name","RFM Demo Stream")
+	w.Header().Set("icy-pub","0")
 	for true {
 		files, err := ioutil.ReadDir("./music")
 		if err != nil {
@@ -115,19 +110,23 @@ func AudiNo(w http.ResponseWriter, r *http.Request) {
 
 		for _, file := range files {
 			fmt.Println("play advert")
-			adv, err := ioutil.ReadFile("./web/audio/advert.m4a")
-			if err != nil {
+			adv,err :=ioutil.ReadFile("./web/audio/advert.m4a")
+			if err!=nil{
 				logerror(err)
 			}
-			binary.Write(w, binary.BigEndian, adv)
+			binary.Write(w, binary.BigEndian,adv)
 			fmt.Println(file.Name())
-			b, err := ioutil.ReadFile("./music/" + file.Name())
-			if err != nil {
+			b,err:=ioutil.ReadFile("./music/"+file.Name())
+			if err!=nil{
 				logerror(err)
 			}
-			binary.Write(w, binary.BigEndian, b)
+			binary.Write(w, binary.BigEndian,b)
 			//flusher.Flush() // Trigger "chunked" encoding
 		}
 		return
 	}
 }
+
+
+
+
